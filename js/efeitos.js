ec079b8f4783a5466894aa8cffb6ba1a95b7e8e4
andregama <eng.andregama@gmail.com>
2017-08-23 16:16:27 -0300

    Ajustes nos efeitos carrossel mobile.

--- js/efeitos.js
+++ js/efeitos.js
@@ -7,34 +7,34 @@ $(document).ready(function(){
     $("#input-search").on("focus",function(){
         $("li.search").addClass("ativo");
     }).on("blur",function(){
-      $("li.search").removeClass("ativo");
+        $("li.search").removeClass("ativo");
     });
-      $(".thumbnails").owlCarousel({
-          items: 4 // Acrescentado na aula 38 para exibir 4 thumbnails de video na seção de vídeos.
+        $(".thumbnails").owlCarousel({
+          //items: 4 // Acrescentado na aula 38 para exibir 4 thumbnails de video na seção de vídeos.
           //loop: true,
           //margin: 10,
           //nav: true,  // Estas duas linhas permitem que apareça os botões Proximo e Anterior no do carrossel.
           //navText: ["Anterior","Proximo"],
-          /*responsive: {
-          //  0: {
-                item: 1
+          responsive: {
+            0: {
+                items: 1
             },
             480: {
-                item: 3
+                items: 3
             },
             768: {
-                item: 4
+                items: 4
             },
             1200: {
-                item: 6
-            }
-          }*/
-      });
-      var owl = $(".thumbnails").data('.owlCarousel');
-      $('#btn-news-prev').on("click", function(){
-        owl.prev();
-      });
-      $('#btn-news-next').on("click", function(){
-        owl.next();
+                items: 6
+            },
+          }
+    });
+        var owl = $(".thumbnails").data('.owlCarousel');
+        $('#btn-news-prev').on("click", function(){
+          owl.prev();
+        });
+        $('#btn-news-next').on("click", function(){
+          owl.next();
       });
   });
