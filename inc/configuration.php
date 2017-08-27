3446b50b3a954a5a7b914154e8b3d7f4e18b6530
andregama <eng.andregama@gmail.com>
2017-08-27 20:02:32 -0300

    Arquivo de configuração (construtores php)

--- inc/configuration.php
+++ inc/configuration.php
@@ -1,3 +1,26 @@
 <?php
 
+class sql {
+
+    public $conn;
+
+    public function__construct() {
+
+        return $this-> conn = mysqli_connect("127.0.0.1", "root", "", "hcode_shop");
+
+    }
+    public function query($string_query) {
+
+        return mysqli_query($this->conn,"$string_query");
+
+    }
+
+    public function__destruct(){
+
+        mysqli_close($this->conn);
+
+    }
+}
+
+
 ?>
