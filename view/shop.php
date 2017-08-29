067a0ec4037d215766566269edbdbb2c301d19c4
andregama <eng.andregama@gmail.com>
2017-08-29 20:17:40 -0300

    Adding Shop.php

---
+++ view/shop.php
@@ -0,0 +1,44 @@
+<!-- Aula 65 - Criação da loja virtual -->
+<?php include_once("header.php"); ?> <!-- Chama o cabeçalho -->
+<section>
+    <div class="container">
+        <div id="destaque-produtos" id="destaque-produtos-container">
+            <div id="destaque-produtos">
+                <div class="item">
+                    <div class="col-sm-6 col-imagem">
+                        <img src="img/produtos/moto-x.png" alt="Nome do Produto">
+                    </div>
+                    <div class="col-sm-6 col-descricao">
+                        <h2>Smartphone Motorola Moto X Play Dual Chip Desbloqueado Android 5.1</h2>
+                        <div class="box-valor">
+                            <div class="text-noboleto text-arial-cinza">no boleto</div>
+                            <div class="text-por text-arial-cinza">por</div>
+                            <div class="text-reais text-roxo">R$</div>
+                            <div class="text-valor text-roxo">1.259</div>
+                            <div class="text-valor-centavos text-roxo">,10</div>
+                            <div class="text-parcelas text-arial-cinza">ou em até 8x de R$ 174,88</div>
+                            <div class="text-total text-arial-cinza">total a prazo R$ 1.399,00</div>
+                        </div>
+                        <a href="#" class="btn btn-comprar text-roxo"><i class="fa fa-shopping-cart"></i>compre agora</a> <!--Retirado da div porque está posicionado num nivel diferente do resto dos textos -->
+                    </div>
+                </div>
+            </div>
+                        <button type="button" id="btn-destaque-prev"><i class="fa fa-angle-left"></i></button>
+                        <button type="button" id="btn-destaque-next"><i class="fa fa-angle-right"></i></button>
+        </div>
+</section>
+<?php include_once("footer.php"); ?> <!-- Aula 64 - Chama o footer no footer.php -->
+
+
+
+
+
+
+
+
+
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>-->
+<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>-->
+<!--https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous"-->
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>-->
+<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script> -->
